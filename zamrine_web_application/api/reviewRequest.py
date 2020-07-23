6aeae1151ccbef7cd6b86057048f4678be1ee65f
noufal <noufal@flogic.in>
2020-07-24 01:45:24 +0530

    create a API for add review

--- zamrine_web_application/api/reviewRequest.py
+++ zamrine_web_application/api/reviewRequest.py
@@ -1,15 +1,19 @@
 from django.http import HttpResponse, JsonResponse
 from ..serializers import ReviewSerializer
-from ..model.review import Reviews
+from ..model.review import Reviews, ReviewForm
 from ..model.product import Product
+import json
+from django.views.decorators.csrf import csrf_exempt
 
+@csrf_exempt
 def reviews(request):
     if request.method == 'GET':
         productID = request.GET.get('product_id')
         if productID is None :
-            response = JsonResponse(data={'status': 'error', 'message':'product id was mandatory'})
+            response = JsonResponse(data={'status': 'error', 'message':'Product ID was mandatory.'})
             response.status_code = 404
             return response
+
         else :
             product = Product.objects.get(id= productID)
             reviews = Reviews.objects.filter(product= product)
@@ -17,4 +21,35 @@ def reviews(request):
 
             return JsonResponse(serializer.data, safe=False)
     
-    # if request.method == 'POST':
+    if request.method == 'POST':
+        requestBody = json.loads(request.body)
+        productID = requestBody.get('id')
+        response = {}
+        if productID is not None :
+            product = Product.objects.get(id = productID)
+            
+            if product is not None :
+                reviewForm = ReviewForm(requestBody)
+                review = reviewForm.save(commit=False)
+                review.product = product
+                reviewForm.save()
+
+                response = JsonResponse(data={'status': 'success', 
+                    'message':'Review is added.'})
+                response.status_code = 201
+
+            else :
+                response = JsonResponse(data={'status': 'success', 
+                    'message':'Please provide valid product ID.'})
+                response.status_code = 403
+
+        else:
+            response = JsonResponse(data={'status': 'error', 
+                'message':'Product ID was mandatory.'})
+            response.status_code = 404
+    
+    return response
+
+
+
+
--- zamrine_web_application/model/review.py
+++ zamrine_web_application/model/review.py
@@ -1,11 +1,17 @@
 from django.db import models
 from .product import Product
+from django import forms
 
 class Reviews(models.Model):
     product = models.ForeignKey(Product, on_delete=models.CASCADE, null=True)
     created_at = models.DateTimeField(auto_now=True)
-    title = models.CharField(max_length=20,null=True)
+    title = models.CharField(max_length=20, null=True)
     image_url = models.URLField(null=True)
-    message = models.TextField(null=True)
-    name = models.CharField(max_length=40, null=True)
+    message = models.TextField(null=False, default="")
+    name = models.CharField(max_length=40, null=False, default="")
     rating = models.DecimalField(decimal_places=2, max_digits=3)
+
+class ReviewForm(forms.ModelForm):
+    class Meta:
+        model = Reviews
+        fields = ['title', 'image_url', 'message', 'name', 'rating']
